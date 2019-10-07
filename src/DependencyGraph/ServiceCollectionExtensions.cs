a9175c2f215e861776cd13561371f5ad8d494c77
lanceccraig <lancecraig513@outlook.com>
2019-10-07 19:39:50 -0400

    Add service collection extensions

---
+++ src/DependencyGraph/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿// -------------------------------------------------------------------------------------------------
+// <copyright file="ServiceCollectionExtensions.cs" company="LanceC">
+// Copyright (c) LanceC. All rights reserved.
+// </copyright>
+// -------------------------------------------------------------------------------------------------
+
+using DependencyGraph.Internal;
+using DependencyGraph.Internal.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DependencyGraph
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDependencyGraph(this IServiceCollection services)
+            => services
+            .AddTransient(typeof(ICycleDetector<>), typeof(CycleDetector<>))
+            .AddTransient(typeof(IGraphFactory<>), typeof(GraphFactory<>))
+            .AddTransient(typeof(IDependencyExecutionEngine<>), typeof(DependencyExecutionEngine<>));
+    }
+}
